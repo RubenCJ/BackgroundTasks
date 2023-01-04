d74634924c94ce07a9d1e88a58d5afa66a68e1f2
Ruben Corona Jacobo <ruben.cj@gmail.com>
2023-01-04 15:51:46 -0600

    Proceso en segundo plano

---
+++ MyBackgroundService.cs
@@ -0,0 +1,27 @@
+﻿namespace BackgroundTasks
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MyBackgroundService> _logger;
+
+        public MyBackgroundService(ILogger<MyBackgroundService> logger)
+        {
+            this._logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(! stoppingToken.IsCancellationRequested) {
+                _logger.LogInformation("From MyBackgroundService: ExecuteAsync {dateTime}", DateTime.Now);
+                await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+            }
+            
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("From MyBackgroundService : StopAsync {dateTime}", DateTime.Now);
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
